e26e3cd3ac71cc4363df8512a6bd820b5eb5c300
Vijay Gora <vijay.gora@rajman.in>
2019-06-10 18:52:14 +0530

    condition

--- demo.php
+++ demo.php
@@ -4,9 +4,7 @@
 	$c=6;
 	if($a>$b&&$a>$c){
 		echo "a is greater then b and c";
-	}elseif($b>$c&&$b>$a){
+	}else($b>$c&&$b>$a){
 		echo "b is greater than a and c";
-	}else{
-		echo "c is greater;
 	}
 ?>
